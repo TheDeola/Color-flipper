a25e2663f793634a2e98ccb90233f83c3b78d74b
TheDeola <aadetunji012@gmail.com>
2022-07-16 13:04:35 +0200

    commit update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const colors = ["green", "rgba(180, 17, 200, 0.5)", "red", "rgba(133,122,200)", "pink", "#f15025"];
 const btn = document.getElementById('btn');
-// const bgColorText = document.querySelector('.bgcolor-text');
+
 const color = document.querySelector('.color')
+const navLink = document.querySelector('.nav-link')
+const bgColorText = document.querySelector('.bgcolor-text');
+
 
 btn.addEventListener('click', function(){
     const randomNumber = getRanNum();
@@ -9,6 +12,9 @@ btn.addEventListener('click', function(){
     document.body.style.backgroundColor=colors[randomNumber];
     color.textContent=colors[randomNumber];
     // document.style.bgColorText=backgroundColor[randomNumber];
+    color.style.color = colors[randomNumber];
+    navLink.style.color = colors[randomNumber];
+    bgColorText.style.color = colors[randomNumber];
 }) 
 const getRanNum = () => {
     return Math.floor(Math.random()*colors.length); 
--- hex.js
+++ hex.js
@@ -2,6 +2,9 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
+const navLink = document.querySelector('.nav-link')
+const bgColorText = document.querySelector('.bgcolor-text');
+
 btn.addEventListener('click', function(){
     let hexColor='#';
     for(let i = 0; i<6; i++){
@@ -9,6 +12,9 @@ btn.addEventListener('click', function(){
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
+    color.style.color = hexColor;
+    navLink.style.color = hexColor;
+    bgColorText.style.color = hexColor;
 })
 
 const getRanNum = ()=>{
